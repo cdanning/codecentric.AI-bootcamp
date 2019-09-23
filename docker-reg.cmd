39195d6146482420a8f53d61cc89a30f8336136d
cdanning <christian.danninger@gmx.net>
2019-09-23 11:37:33 +0200

    nlp 2

---
+++ docker-reg.cmd
@@ -0,0 +1,3 @@
+reg ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v EditionId /t REG_SZ /d Professional /f
+
+reg ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName /t REG_SZ  /d "Windows 10 Pro" /f
--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
 git pull
+git pull https://github.com/codecentric/codecentric.AI-bootcamp.git master 
 docker run -p 127.0.0.1:8888:8888 -v "%cd%/notebooks":/notebooks -v "%cd%/data":/data codecentric/codecentric.ai-docker
+pause
