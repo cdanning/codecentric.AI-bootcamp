df62b7ca20405adbc269d51ff082f5a84f6b6153
cdanning <christian.danninger@gmx.net>
2019-10-06 15:09:09 +0200

    6.10.2019

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
+echo "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 git pull
 git pull https://github.com/codecentric/codecentric.AI-bootcamp.git master 
 docker run -p 127.0.0.1:8888:8888 -v "%cd%/notebooks":/notebooks -v "%cd%/data":/data codecentric/codecentric.ai-docker
 pause
+git push
+cmd /K
